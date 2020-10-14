13ed0cb62707042c7b5aebe15a3df44388d47e87
yoad-ashuri <yoad.ashuri2112@gmail.com>
2020-10-14 18:41:16 +0300

    start to create the JS file

---
+++ game.js
@@ -0,0 +1,37 @@
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+
+$(".btn").on("click", function(){
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
+
+function nextSequence() {
+  var randomNumber = Math.floor((Math.random()*4));
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+  // var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  // audio.play();
+}
+
+function playSound(name){
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function() {
+    $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
+
+$("body").on("keypress",function(){
+  nextSequence();
+});
