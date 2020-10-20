ab75e07e26762c07e86135b9395400ca268a2de5
yoad-ashuri <yoad.ashuri2112@gmail.com>
2020-10-20 17:08:38 +0300

    final code

--- game.js
+++ game.js
@@ -1,30 +1,48 @@
-
+//varibles decleretion
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var start = false;
+var level = 0;
 
+//onClick func for the buttons
 $(".btn").on("click", function(){
+  if(start){
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
   animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
+}});
+
+//init the game
+$("body").on("keypress",function(){
+  if (!(start)) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    start = true;
+  }
 });
 
+//func that generate the next sequence
 function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
   var randomNumber = Math.floor((Math.random()*4));
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
-  // var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-  // audio.play();
 }
 
+//func that make a sound according to the pressed button
 function playSound(name){
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
+//func that make press animation
 function animatePress(currentColour) {
   $("#" + currentColour).addClass("pressed");
   setTimeout(function() {
@@ -32,6 +50,32 @@ function animatePress(currentColour) {
     }, 100);
 }
 
-$("body").on("keypress",function(){
-  nextSequence();
-});
+
+//func that check the user Answer
+function checkAnswer(currentLevel) {
+if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+  console.log("success");
+  if ( (currentLevel + 1) === gamePattern.length ) {
+    setTimeout(function(){
+      nextSequence();
+    }, 1000);
+  }
+
+} else {
+  console.log("wrong");
+  playSound("wrong");
+  $("body").addClass("game-over");
+  setTimeout(function() {
+    $("body").removeClass("game-over");
+    }, 200);
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+  startOver();
+}
+}
+
+//func that restart the gave and at the next key press the game will restart
+function startOver() {
+  gamePattern = [];
+  start = false;
+  level = 0;
+}
